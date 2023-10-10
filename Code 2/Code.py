04e1254b948d39f0106965219fa19e21987253cf
Azd@n | އަޒްދާން <113350077+A204N2007@users.noreply.github.com>
2023-10-09 20:28:56 -0800

    Create Code.py

---
+++ Code 2/Code.py
@@ -0,0 +1,6 @@
+#Finding the total amount of student presented in grade 10 total
+a=int(input("Enter the amount of students presented today in Class 10A:"))
+b=int(input("Enter the amount of students presented today in Class 10B:"))
+c=int(input("Enter the amount of students presented today in Class 10C:"))
+sum=int(a)+int(b)+int(c)
+print("The sum is:", sum)
