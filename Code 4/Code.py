a610716258b6f94fc49161be5cdc2c150d89d292
Azd@n | އަޒްދާން <113350077+A204N2007@users.noreply.github.com>
2023-10-10 19:49:03 -0800

    Create Code.py

---
+++ Code 4/Code.py
@@ -0,0 +1,11 @@
+MyCountry=input("Enter the name of your country: ")
+Length=len(MyCountry)
+FirstThree=MyCountry[:3]
+LastFive=MyCountry[-5:]
+UpperCase=MyCountry .upper()
+LowerCase=MyCountry .lower()
+print("The length of the country name is", Length)
+print("The first three characters of the country name are", FirstThree)
+print("The last five characters of the country name are", LastFive)
+print("The country name in upper case is", UpperCase)
+print("The country name in lower case is", LowerCase)
